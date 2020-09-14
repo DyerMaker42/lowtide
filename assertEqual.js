79f9e1c6680939f9f265468d108504e0443d2252
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-14 21:31:19 +0000

    added two additinoal test cases to check edges outlined

--- assertEqual.js
+++ assertEqual.js
@@ -18,4 +18,6 @@ const assertEqual = function (actual, expected) {
 
 // test code
 assertEqual("LightHouse Labs", "Bootcamp");
+assertEqual(1, 2);
+assertEqual("LightHouse Labs", "LightHouse Labs");
 assertEqual(1, 1);

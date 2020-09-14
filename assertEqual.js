1d754e8ef94fe31617337687780e0af88d54a5a9
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-14 21:30:25 +0000

    added logic to assert equal

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,21 @@
 // function implementation
 
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
+  let result = "";
 
-
+  if (actual === expected) {
+     result = true;
+  } else {
+     result = false;
+  };
+  if (result) {
+    console.log(`Assertion Passed:${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed:${actual} !== ${expected}`);
+  };
 
 };
 
 // test code
 assertEqual("LightHouse Labs", "Bootcamp");
-assertEqual(1,1);
+assertEqual(1, 1);

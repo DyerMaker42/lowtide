6b98636ed36c5fa804f6e7cbc3faa27440e6eb10
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-15 23:38:25 +0000

    adding test cases

--- without.js
+++ without.js
@@ -17,3 +17,27 @@ const assertArraysEqual = function (actualArray, expectedArray) {
     console.log(`🤬😰🛑Assertion Failed:${actualArray} !== ${expectedArray}`);
   }
 }
+
+
+const without = function (source, itemsToRemove) {
+  if (itemsToRemove === []) {
+    return source
+  } else if (eqArrays(source,itemsToRemove)) {
+    return []
+  }
+  //check each element in items to remove array is within source array
+  for (let i = 0; i < source.length; i ++){
+    for (let j = 0; j< itemsToRemove.length; j++){
+      console.log(i,j)
+    }
+  }
+  // compare items between each
+
+  //push items from source that don't match to output array
+
+  return output
+}
+
+assertArraysEqual(without([1,2,3],[1,2,3]),[]);
+assertArraysEqual(without([1,2,3],[1]),[2,3]);
+assertArraysEqual(without)

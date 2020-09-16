3903e3271f821ea46f19cbe345d2ec050c70743d
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-16 15:56:09 +0000

    implemented logic for count letters

---
+++ countLetters.js
@@ -0,0 +1,37 @@
+const assertEqual = function (actual, expected) {
+  let result = "";
+
+  if (actual === expected) {
+    result = true;
+  } else {
+    result = false;
+  }
+  if (result) {
+    console.log(`😇😍✅Assertion Passed:${actual} === ${expected}`);
+  } else {
+    console.log(`🤬😰🛑Assertion Failed:${actual} !== ${expected}`);
+  }
+
+};
+
+const countLetters = (input) => {
+  let results = {};
+  for (letter of input) {
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter] += 1
+      } else {
+        results[letter] = 1
+      }
+    }
+  }
+  return results
+}
+
+console.log(countLetters("hotwire"));
+countLetters("sassafrass");
+console.log(countLetters("sassafrass"))
+countLetters("lighthouse");
+console.log(countLetters("lighthouse"))
+console.log(countLetters("lighthouse in the house"))
+countLetters("123");

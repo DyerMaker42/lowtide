caf3716c0f847d2d1b94d0a972a09d3b7a18b003
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-16 16:20:46 +0000

    linted letter positions

--- letterPositions.js
+++ letterPositions.js
@@ -1,41 +1,41 @@
-const eqArrays = function (a, b) {
-  if (a&&b){
-  if (a.length !== b.length) {
-    return false
-  }
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      return false
+const eqArrays = function(a, b) {
+  if (a && b) {
+    if (a.length !== b.length) {
+      return false;
     }
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
   }
-  return true
-}
-}
+};
 
-const assertArraysEqual = function (actualArray, expectedArray) {
+const assertArraysEqual = function(actualArray, expectedArray) {
   if (eqArrays(actualArray, expectedArray)) {
     console.log(`😇😍✅Assertion Passed:${actualArray} === ${expectedArray}`);
   } else {
     console.log(`🤬😰🛑Assertion Failed:${actualArray} !== ${expectedArray}`);
   }
-}
+};
 
 
 
 const letterPositions = (sentence) => {
   const results = {};
-  for (let i = 0; i < sentence.length; i++){
-    if (sentence[i] !== " "){
-      if (results[sentence[i]]){
-        results[sentence[i]].push(i)
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
       } else {
-        results[sentence[i]] = [i]
+        results[sentence[i]] = [i];
       }
     }
   }
-  return results
-}
+  return results;
+};
 
-console.log(letterPositions("lighthouse in the house"))
+console.log(letterPositions("lighthouse in the house"));
 // assertArraysEqual(letterPositions('Hello')['l'], [2,3])
 // assertArraysEqual(letterPositions('lighthouse in the house').i, [1,11])

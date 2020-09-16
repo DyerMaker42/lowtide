fbf1a220460d38a1893cc8bc5548316e4143795c
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-16 22:20:09 +0000

    commented out old console.logs

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -43,49 +43,51 @@ const eqObjects = (obj1, obj2) => {
     //console.log("TEST",obj2[key], obj1[key])
     //check same keys in each
     if (!obj2[key]) {
-      console.log("if 1");
+      // console.log("if 1");
       return false;
     }
 
     //check same key values in each
     //comparing arrays if value is an array
     else if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
-      console.log("if 2");
-      console.log(obj1[key]);
+      // console.log("if 2");
+      // console.log(obj1[key]);
       if (!eqArrays(obj1[key], obj2[key])) {
-        console.log("if 3");
+        // console.log("if 3");
         return false;
       }
     } else if (obj1[key] !== obj2[key]) {
-      console.log("if 4");
+      // console.log("if 4");
       return false;
     }
   }
   return true;
 };
 
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertEqual(eqObjects(ab, ba), true);
-// const abc = { a: 1, b: "2", c: 3 };
-// const abx = { a: 1, b: 2, x: 3 };
-// const aby = { a: 1, b: 2, c: "y" };
-// const abt = { a: "1", b: "2", c: "3" };
-// assertEqual(eqObjects(abc, abc), true);
-// assertEqual(eqObjects(abc, aby), false);
-// assertEqual(eqObjects(abc, aby), false);
-// assertEqual(eqObjects(abc, aby), false);
-// assertEqual(eqObjects(ab, abt), false);
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertEqual(eqObjects(cd, dc), true);
-// assertEqual(eqObjects(cd, cd2), false);
 
 const assertObjectsEqual = (actual, expected) => {
+  const inspect = require('util').inspect;
   if ((eqObjects(actual, expected))) {
     console.log(`😇😍✅Assertion Passed:${actual} === ${expected}`);
   } else {
     console.log(`🤬😰🛑Assertion Failed:${actual} !== ${expected}`);
   }
 }
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: 1, b: "2", c: 3 };
+const abx = { a: 1, b: 2, x: 3 };
+const aby = { a: 1, b: 2, c: "y" };
+const abt = { a: "1", b: "2", c: "3" };
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(eqObjects(ab, ba), true);
+assertObjectsEqual(eqObjects(abc, abc), true);
+assertObjectsEqual(eqObjects(abc, aby), false);
+assertObjectsEqual(eqObjects(abc, aby), false);
+assertObjectsEqual(eqObjects(abc, aby), false);
+assertObjectsEqual(eqObjects(ab, abt), false);
+assertObjectsEqual(eqObjects(cd, dc), true);
+assertObjectsEqual(eqObjects(cd, cd2), false);

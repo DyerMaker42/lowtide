a589be2dcf2e29ff8a1071a1b78c98c26e5c6c14
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-16 19:27:05 +0000

    added logic to find key value

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -13,3 +13,28 @@ const assertEqual = (actual, expected)  => {
   }
 
 };
+
+const findKeybyValue = (object, value) => {
+let results= undefined
+for ( let keys in object){
+  if (object.hasOwnProperty(keys)){
+    if (object[keys] === value){
+      results = keys
+    }
+  }
+}
+
+
+return results
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeybyValue(bestTVShowsByGenre,"The Wire"), "drama")
+assertEqual(findKeybyValue(bestTVShowsByGenre,"That '70s Show"), undefined)
+assertEqual(findKeybyValue(bestTVShowsByGenre,"Brooklyn Nine-Nine"), "comedy")
+assertEqual(findKeybyValue(bestTVShowsByGenre,"The Expanse"), "sci_fi")

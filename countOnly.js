0489f04a5e42f99aed81b87e2f84c6e629402c00
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-16 02:40:57 +0000

    added test logic to countOnly

---
+++ countOnly.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  let result = "";
+
+  if (actual === expected) {
+    result = true;
+  } else {
+    result = false;
+  }
+  if (result) {
+    console.log(`😇😍✅Assertion Passed:${actual} === ${expected}`);
+  } else {
+    console.log(`🤬😰🛑Assertion Failed:${actual} !== ${expected}`);
+  }
+
+};
+
+const countOnly = (allItems, itemsToCount) => {
+  const results = {};
+  
+  
+ 
+ 
+ 
+  return results
+}
+
+//test code
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);

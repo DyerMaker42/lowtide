25dae51be81f798214d1b1e9637f299967578f76
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-17 15:56:59 +0000

    linted findKey

--- findKey.js
+++ findKey.js
@@ -17,13 +17,13 @@ const assertEqual = (actual, expected)  => {
 const findKey = (object, callback) => {
   let result = undefined;
   for (let key in object) {
-    if(callback(object[key])){
+    if (callback(object[key])) {
       result = key;
-      return result
+      return result;
     }
   }
-  return result
-}
+  return result;
+};
 
 
 const test = {
@@ -35,4 +35,4 @@ const test = {
   "Akelarre":  { stars: 3 }
 };
 
-console.log(findKey(test, x => x.stars === 3))
+console.log(findKey(test, x => x.stars === 3));

f97dbe864a89067c2d26073aae9d567cc2bc9102
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-17 01:26:50 +0000

    updated logic

--- map.js
+++ map.js
@@ -3,10 +3,14 @@
 
 
 const map = (array, callback) => {
-  console.log(array, "array")
-  console.log(callback, "callback");
-  
   const results = [];
+  for(let item of array){
+   results.push(callback(item))
+  } 
+  
+  
+  
+  
   return results
 
 }

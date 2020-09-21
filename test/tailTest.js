7b1a658613b4ccd435480f5539f9fa74a3c1fe9c
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-21 23:30:23 +0000

    refactored tail test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,21 @@
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 const assertEqual = require("../assertEqual")
 const tail = require("../tail")
+const assert = require("chai").assert;
 
 
-assertEqual(words.length, 3);
-assertEqual(tail([1]), []);
-assertEqual(tail([0,1,2]), [1,2]);
+
+// assertEqual(words.length, 3);
+// assertEqual(tail([1]), []);
+// assertEqual(tail([0,1,2]), [1,2]);
+
+describe('#tail', () => {
+  it(" returns empty array for array with one value", () => {
+    assert.deepEqual(tail([1]), [])
+  });
+
+  it(" returns [1,2] array for [0,1,2]", () => {
+    assert.deepEqual(tail([0, 1, 2]), [1, 2])
+  });
+
+})

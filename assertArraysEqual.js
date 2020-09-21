3d82c7c503930c261cac6178cd0ce22f298b5a0b
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-21 21:27:55 +0000

    added arrow function to AAE

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 const eqArrays = require("./eqArrays")
 
-const assertArraysEqual = function (actualArray, expectedArray) {
+const assertArraysEqual = (actualArray, expectedArray) => {
   if (eqArrays(actualArray, expectedArray)) {
     console.log(`😇😍✅Assertion Passed:${actualArray} === ${expectedArray}`);
   } else {

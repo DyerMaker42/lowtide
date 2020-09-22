b39eae2035852d53781c1fbc8903d0322fa24fde
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-22 01:48:48 +0000

    added more fucntions to index

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require("./middle");
+const assertArraysEqual =require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters
 
 
 module.exports = {

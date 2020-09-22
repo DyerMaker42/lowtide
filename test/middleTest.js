baa33716e463303e481597d033255dd4e5a97a2f
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-22 01:34:53 +0000

    refactored middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,44 @@
 const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
-
+const assert = require('chai').assert;
 
 assertArraysEqual(middle([1, 2]), []);
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle(["a", 1, 2]), [1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+describe('#middle', () => {
+  it("returns empty array when given two value array", () => {
+    assert.deepEqual(middle([1, 2]), [])
+  });
+  it("returns [2] when given [1,2,3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2])
+  });
+
+  it('returns [1] when given ["a", 1, 2]', () => {
+    assert.deepEqual(middle(["a", 1, 2]), [1])
+  });
+
+  it("returns [2,3] when given [1,2,3,4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
+  });
+
+  
+
+})
